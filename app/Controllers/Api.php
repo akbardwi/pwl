028c76a5e78ed734712219fe11eef66fe1f4faf0
Akbar Dwi Syahputra <akbar.dwi14@gmail.com>
2021-06-04 14:40:45 +0700

    Penambahan RESTful API

---
+++ app/Controllers/Api.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Controllers;
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+
+// Load Model
+use App\Models\Barang_model;
+use App\Models\Pelanggan_model;
+// End Load Model
+
+class Api extends ResourceController{
+    use ResponseTrait;
+    
+    //Fungsi Ambil Semua Data
+	public function getData(){
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method != "GET"){
+            $data = [
+                "reason"    => "Method tidak diizinkan"
+            ];
+            return json_encode($data);
+        }
+		$model = new Pelanggan_model();
+		$data = $model->findAll();
+        return $this->respond($data, 200);
+	}
+
+    // Fungsi Ambil 1 Data
+    public function show($id = null){
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method != "GET"){
+            $data = [
+                "reason"    => "Method tidak diizinkan"
+            ];
+            return json_encode($data);
+        }
+        $model = new Pelanggan_model();
+        $data = $model->getWhere(['id' => $id])->getResult();
+        if($data){
+            return $this->respond($data);
+        }else{
+            return $this->failNotFound('Data tidak ditemukan');
+        }
+    }
+
+    // Fungsi Tambah Data
+    public function create(){
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method != "POST"){
+            $data = [
+                "reason"    => "Method tidak diizinkan"
+            ];
+            return json_encode($data);
+        }
+        $model = new Pelanggan_model();
+        $data = $this->request->getPost();
+        $model->tambah($data);
+        $response = [
+            'status'   => 201,
+            'error'    => null,
+            'messages' => [
+                'success' => 'Data disimpan'
+            ]
+        ];
+         
+        return $this->respondCreated($response, 201);
+    }
+
+    // Fungsi Update Data
+    public function update($id = null)
+    {
+        $model = new Pelanggan_model();
+        $data = $this->request->getPost();
+        $check = $model->check_code($data['kode']);
+
+        // Insert to Database
+        $model->update($id, $data);
+        $response = [
+            'status'   => 200,
+            'error'    => null,
+            'messages' => [
+                'success' => 'Data diupdate'
+            ]
+        ];
+        return $this->respond($response);
+    }
+
+    // delete product
+    public function delete($id = null){
+        $model = new Pelanggan_model();
+        $data = $model->find($id);
+        if($data){
+            $model->delete($id);
+            $response = [
+                'status'   => 200,
+                'error'    => null,
+                'messages' => [
+                    'success' => 'Data dihapus'
+                ]
+            ];
+             
+            return $this->respondDeleted($response);
+        }else{
+            return $this->failNotFound('Data tidak ditemukan');
+        }
+         
+    }
+}
